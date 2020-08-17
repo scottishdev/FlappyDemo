13cd34adea243bafaa613eb77da80d4f63bd19f2
Tartan Developer <pchu192@gmail.com>
2020-08-17 16:30:35 +0100

    implemented tubes in playstate - working

--- core/src/com/github/scottishdev/game/States/PlayState.java
+++ core/src/com/github/scottishdev/game/States/PlayState.java
@@ -5,16 +5,19 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.github.scottishdev.game.FlappyDemo;
 import com.github.scottishdev.game.sprites.Bird;
+import com.github.scottishdev.game.sprites.Tube;
 
 public class PlayState extends State{
     private Bird bird;
     private Texture bg;
+    private Tube tube;
 
     public PlayState(GameStateManager gsm) {
         super(gsm);
         bird = new Bird(50,300);
         cam.setToOrtho(false, FlappyDemo.WIDTH / 2, FlappyDemo.HEIGHT / 2);
         bg = new Texture("bg.png");
+        tube = new Tube(100);
     }
 
     @Override
@@ -36,6 +39,8 @@ public class PlayState extends State{
         sb.begin();
         sb.draw(bg, cam.position.x - (cam.viewportWidth/2),0);
         sb.draw(bird.getTexture(), bird.getPosition().x, bird.getPosition().y);
+        sb.draw(tube.getTopTube(), tube.getPositionTopTube().x, tube.getPositionTopTube().y);
+        sb.draw(tube.getBottomTube(), tube.getPositionBottomTube().x, tube.getPositionBottomTube().y);
         sb.end();
     }
 
