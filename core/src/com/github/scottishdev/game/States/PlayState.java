e2c03dfebd0ed980fb31f5a134070c69d75f1332
Tartan Developer <pchu192@gmail.com>
2020-08-17 15:25:23 +0100

    bg.png rendered once game start button is clicked.

--- core/src/com/github/scottishdev/game/States/PlayState.java
+++ core/src/com/github/scottishdev/game/States/PlayState.java
@@ -8,11 +8,13 @@ import com.github.scottishdev.game.sprites.Bird;
 
 public class PlayState extends State{
     private Bird bird;
+    private Texture bg;
 
     public PlayState(GameStateManager gsm) {
         super(gsm);
         bird = new Bird(50,300);
         cam.setToOrtho(false, FlappyDemo.WIDTH / 2, FlappyDemo.HEIGHT / 2);
+        bg = new Texture("bg.png");
     }
 
     @Override
@@ -32,6 +34,7 @@ public class PlayState extends State{
     public void render(SpriteBatch sb) {
         sb.setProjectionMatrix(cam.combined);
         sb.begin();
+        sb.draw(bg, cam.position.x - (cam.viewportWidth/2),0);
         sb.draw(bird.getTexture(), bird.getPosition().x, bird.getPosition().y);
         sb.end();
     }
